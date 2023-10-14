d274510b962989290019436039aaa7b549679cc9
May <supunnika-789@hotmail.com>
2023-10-14 22:01:05 +0700

    feat: add landing page and footer component

--- src/app/component/Footer.js
+++ src/app/component/Footer.js
@@ -1,9 +1,44 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <>
-      <div className='bg-[url("https://hzhhjgfmkxwvwsdteigk.supabase.co/storage/v1/object/public/user-storage/icons/bg1-landingPage.svg")] w-screen'></div>
+      <footer className="bg-gray-100 w-[100%] h-[371px] flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-2xl font-medium cursor-pointer">
+            Merry
+            <span className="text-2xl text-red-500 font-extrabold">Match</span>
+          </p>
+          <p className="text-gray-700 font-semibold text-xl mt-5">
+            New generation of online dating website for everyone
+          </p>
+          <hr className="w-[78%] h-[2px] mt-8 mb-3" />
+          <p className="text-sm text-gray-600 font-medium">
+            copyright ©2022 merrymatch.com All rights reserved
+          </p>
+          <div className="flex mt-3">
+            <Link href="https://web.facebook.com/?_rdc=1&_rdr" target="_blank">
+              <img
+                src="https://hzhhjgfmkxwvwsdteigk.supabase.co/storage/v1/object/public/user-storage/icons/fb.svg"
+                className="mr-2"
+              />
+            </Link>
+            <Link href="https://www.instagram.com/" target="_blank">
+              <img
+                src="https://hzhhjgfmkxwvwsdteigk.supabase.co/storage/v1/object/public/user-storage/icons/ig.svg"
+                className="mr-2"
+              />
+            </Link>
+            <Link
+              href="https://twitter.com/i/flow/login?input_flow_data=%7B%22requested_variant%22%3A%22eyJsYW5nIjoidGgifQ%3D%3D%22%7D"
+              target="_blank"
+            >
+              <img src="https://hzhhjgfmkxwvwsdteigk.supabase.co/storage/v1/object/public/user-storage/icons/tw.svg" />
+            </Link>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
--- src/app/component/Navbar.js
+++ src/app/component/Navbar.js
@@ -1,24 +1,27 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <>
       <div className="fixed left-0 top-0 right-0 z-10">
         <div className="h-[88px] flex justify-between px-16 shadow-md bg-main">
-          <p className="text-2xl font-medium flex items-center cursor-pointer">
-            Merry{" "}
+          <p className="text-2xl font-medium flex items-center cursor-pointer hover:scale-110 duration-300">
+            Merry
             <span className="text-2xl text-red-500 font-extrabold">Match</span>
           </p>
           <div className="flex items-center">
-            <p className="text-Purple-800 font-bold px-6 py-8 cursor-pointer">
+            <p className="text-Purple-800 font-bold px-6 py-8 cursor-pointer hover:text-red-600">
               Why Merry Match?
             </p>
-            <p className="text-Purple-800 font-bold px-6 py-8 cursor-pointer">
+            <p className="text-Purple-800 font-bold px-6 py-8 cursor-pointer hover:text-red-600">
               How to Merry
             </p>
-            <button className="bg-red-500 text-white font-bold w-[90px] h-[48px] rounded-3xl">
-              Login
-            </button>
+            <Link href={"/login"}>
+              <button className="bg-red-500 text-white font-bold w-[90px] h-[48px] rounded-3xl hover:bg-red-400">
+                Login
+              </button>
+            </Link>
           </div>
         </div>
       </div>
